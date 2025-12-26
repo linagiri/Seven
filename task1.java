8654618198e215626992f0b033bc9339852f4a09
linagiri <np03cs4a240320@heraldcollege.edu.np>
2025-12-26 13:14:42 +0545

    My task1

--- task1.java
+++ task1.java
@@ -1,5 +1,15 @@
 package Week7Tuto;
 
 public class task1 {
-
+	
+                             
+}
+class myName{
+	public void name() {
+		System.out.println("Lina Giri");
+	}
+	public void studentName() {
+		System.out.println("Lina");
+		
+	}
 }
